148e57436d4604764758354f15799dc2ed2fba45
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2024-02-27 00:18:19 +0530

    Created a new middleware  and  added login and logout method while using cookie

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,24 @@ import { ApiError } from "../utils/ApiError.js";
 import {User} from "../models/user.models.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
+
+const generateAccessAndRefreshTokens = async(userId) =>
+ {
+     try {
+          const user = await User.findById(userId)
+          const accessToken = user.generateAccessToken
+          const refreshToken = user.generateRefreshToken
+
+          user.refreshToken = refreshToken
+          await user.save({ validateBeforeSave: false }) //while saving in db, mongoose model starts 
+                                                        //kickin eg password is also required but 
+                                                  //validateBeforeSave has been false for this reason
+          return {accessToken, refreshToken}
+     } catch (error) {
+          throw new ApiError(500,"Something went wrong while generating Refresh and Access token")
+     }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
      // get user details from fronted
      // validation - not empty
@@ -74,5 +92,89 @@ const registerUser = asyncHandler( async (req, res) => {
       )
 })
 
+const loginUser = asyncHandler( async (req, res) =>{
+     // bring data from req body   (req body -> data)
+     //username or email
+     //find the user
+     //password check 
+     //access and refresh token generation
+     //send cookie
 
-export {registerUser}
+     const {email, username, password} = req.body
+
+     if (!(username || email)) {
+          throw new ApiError(400, "username or email is required")      
+     }
+
+     const user = await User.findOne({
+          $or: [{username}, {email}]
+     })
+
+     if (!user) {
+          throw new ApiError(404,"User does not exist")
+     }
+
+     const isPasswordValid = await user.isPasswordCorrect(password)
+
+     if (!isPasswordValid) {
+          throw new ApiError(40,"Invalid user credentials")
+     }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+     const loggedInUser = await User.findById(user._id).select("-password - refreshToken")//if you want to do like this or can update user directly
+
+     const options = {          //secure as this cookie can only be modified from server 
+          httpOnly: true,
+          secure: true
+     }
+
+     return res
+     .status(200)
+     .cookie("accessToken", accessToken, options)
+     .cookie("refreshToken", refreshToken, options)
+     .json(                   //json response
+          new ApiResponse(
+                    200,{
+                         user: loggedInUser, 
+                         accessToken,
+                         refreshToken   //here we are adding again access and refresh token 
+                                        //user want to save access and refresh token
+                                        //depend on what purpose you are doing this
+                         
+                    },
+                    "User logged in Successfully"
+               )
+          )
+
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+          req.user._id,
+          {
+               $set:{
+                    refreshToken: undefined
+               }
+          },
+          {
+               new: true      
+          }   
+          )
+          const options = {          //secure as this cookie can only be modified from server 
+               httpOnly: true,
+               secure: true
+          }
+
+     return res
+     .status(200)
+     .clearCookie("accessToken", options)
+     .clearCookie("refreshToken", options)
+     .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export {
+     registerUser,
+     loginUser,
+     logoutUser
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(
@@ -18,5 +19,11 @@ router.route("/register").post(
     registerUser
     )
 
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser) //For this reaseon we write next() same in 
+                                      //auth.middleware.js we have written next() so another 
+                                      //middleware in .post( , , ) could execute
 
 export default router
