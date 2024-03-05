905f6dad6d70b7a6da98b564d7a3a39dd06b43d7
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2024-03-05 23:50:47 +0530

    deprecated  ObjectId form has been changed into new form

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -400,7 +400,8 @@ const getWatchHistory = asyncHandler( async(req, res) => {
      const user = await User.aggregate([
           {
                $match: {
-                    _id: new mongoose.Types.ObjectId(req.user._id)
+                    _id: mongoose.Types.ObjectId.createFromTime(req.user._id)
+ // new mongoose.Types.ObjectId() with the recommended mongoose.Types.ObjectId.createFromTime()
                } 
           },
           {
