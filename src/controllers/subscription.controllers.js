a53cb7ede30c3fe4ecac697d1152f12a0d0a53f8
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2024-03-09 01:08:57 +0530

    Add subscription and tweet controllers

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -9,16 +9,96 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const channel =  await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const user = await User.findById(req.user?._id)
+    if (!user) {
+        throw new ApiError(404, "Subscriber not found")
+    }
+
+    const subscription = await Subscription.findOne(
+        {subscriber: user._id, channel: channel._id}
+    )
+
+    if(subscription){
+        await subscription.remove()
+
+        return (res.status(200).json(new ApiResponse(200, null, "Unsubscribed successfully")))
+
+    } else {
+        await Subscription.create(
+            {
+                subscriber: user._id,
+                channel: channel._id
+            }
+        )
+
+        return (res.status(200).json(new ApiResponse(200, null, "Subscribed successfully")))
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const channel  = await User.findById(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const subscribers = await Subscription.find({channel: channel?._id}).populate("subscriber")
+    if (!subscribers || subscribers.length === 0) {
+        throw new ApiError(404, "No subscribers found")
+    }
+    const subscribersList = subscribers.map(subscriber => subscriber.subscriber)
+    const subscribersCount = subscribersList.length
+
+    return (   
+         res
+         .status(200)
+         .json(new ApiResponse(200, {subscribers: subscribersList, subscribersCount}, "Subscribers found successfully"))
+        )     
+   
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber id")
+    }
+
+    const user = await User.findById(subscriberId)
+    if (!user) {
+        throw new ApiError(404, "Subscriber not found")
+    }
+
+    const subscriptions = await Subscription.find({subscriber: user?._id}).populate("channel")
+    if (!subscriptions || subscriptions.length === 0) {
+        throw new ApiError(404, "No subscriptions found")
+    }
+    const subscribedChannels = subscriptions.map(subscription => subscription.channel)
+    const subscribedChannelsCount = subscribedChannels.length
+
+    return (
+        res
+        .status(200)
+        .json(new ApiResponse(200, {subscriptions: subscribedChannels, subscribedChannelsCount}, "Subscribed channels found successfully"))
+    )
+
 })
 
 export {
--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -7,18 +7,102 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body
+    const user = await User.findById(req.user?._id)
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+    const tweet = await Tweet.create({
+        content,
+        owner: user._id
+    })
+
+    return(
+        res
+        .status(201)
+        .json(new ApiResponse(201, tweet, "Tweet created successfully"))
+    )
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const user = await User.findById(req.user?._id)
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+    const userTweet = await Tweet.find({ owner: user._id })
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, userTweet, "User tweets fetched successfully"))
+    )
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const { tweetId } = req.params
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet  = await Tweet.findById(tweetId)
+    if (!tweet) {
+        throw new ApiError(400, "Tweet is not found")
+    }
+
+    const user = await User.findById(req.user?._id)
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    if (tweet.owner.toString() !== user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update this tweet")
+    }
+
+    const content = req.body
+    if (!content) {
+        throw new ApiError(400, "Content is required")
+    }
+
+    tweet.content = content
+    await tweet.save({validateBeforeSave: false})
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "Tweet updated successfully"))
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const tweetId = req.params
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    const user = await User.findById(req.user?._id)
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    if (tweet.owner.toString() !== user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this tweet")
+    }
+
+    tweet.remove()
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, null, "Tweet deleted successfully"))
+    )
+
+
 })
 
 export {
