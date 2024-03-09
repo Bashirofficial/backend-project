57089e75c5a000d291bf75673021943d42285eaa
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2024-03-10 00:35:08 +0530

    Add like functionality for videos, comments, and tweets

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -3,26 +3,119 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {Video} from "../models/video.model.js"
+import {Comment} from "../models/comment.model.js"
+import {Tweet} from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    const like = await Like.findOne({
+        likedBy: req.user?._id,
+        video: videoId
+    })
+
+    if (like) {
+        await like.remove()
+        return new ApiResponse(200, "Video unliked").send(res)
+    } else {
+        await Like.create({
+            likedBy: req.user?._id,
+            video: videoId
+        })
+        return new ApiResponse(200, "Video liked").send(res)
+    }
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id")
+    }
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    const commentLike = await Like.findOne({
+        LikedBy: req.user?._id,
+        comment: commentId
+    })
+
+    if(commentLike){
+        await commentLike.remove()
+        return new ApiResponse(200, "Comment unliked").send(res)
+    } else {
+        await Like.create({
+            likedBy: req.user?._id,
+            comment: commentId
+        })
+        return new ApiResponse(200, "Comment liked").send(res)
+    }
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id")
+    }
+    const tweet  = await Tweet.findById(tweet)
+    if(!tweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    const tweetLike = await Like.findOne({
+        likedBy: req.user?._id,
+        tweet: tweetId
+    })
+
+    if (tweetLike){
+        await tweetLike.remove()
+        return new ApiResponse(200, "Tweet unliked").send(res)
+    } else {
+        await Like.create({
+            likedBy: req.user?._id,
+            tweet: tweetId
+        })
+        return new ApiResponse(200, "Tweet liked").send(res)
+    }
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+   const user = req.user?._id
+   if (!user){
+       throw new ApiError(401, "Unauthorized")
+   }
+   const likes = await Like.find({likedBy: user, video: { $exists: true }}).populate("video")
+   if (!likes){
+       throw new ApiError(404, "No liked videos found")
+   }
+   //If you want the final response to contain just the video objects themselves, without any
+   // additional fields from the like objects, then you should include this line. 
+   //However, if you are interested in additional fields from the like objects or if you want to 
+   //maintain the structure of the like objects, you can omit this line and return the likes array
+   // directly.
+
+   const likedVideos = likes.map(like=> like.video)
+
+   return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, likedVideos, "Liked videos retrieved"))
+    )
 })
 
 export {
--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -3,43 +3,212 @@ import {Playlist} from "../models/playlist.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {User} from "../models/user.model.js"
+import {Video} from "../models/video.model.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
     //TODO: create playlist
+    if (!(name) ) {
+        throw new ApiError(400, "Name is required")
+    }
+
+    const playlist = await Playlist.create({
+        name:name,
+        description: description,
+        owner: req.user?._id  
+    })
+
+    if (!playlist) {
+        throw new ApiError(500, "Playlist not created")
+    }
+
+    return(
+        res
+        .status(201)
+        .json(new ApiResponse(201, playlist, "Playlist created successfully"))
+    )
+
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id")
+    }
+    const user = await User.findById(userId)
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    const playlists = await Playlist.find({owner: userId})
+    if (!playlists) {
+        throw new ApiError(404, "Playlists not found")
+    }
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, playlists, "Playlists fetched successfully"))
+    )
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+    
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist id")
+    }
+
+    const playlist = await Playlist.findById(playlistId)    
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, playlist, "Playlist fetched successfully"))
+    )
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+    if (!isValidObjectId(playlistId) || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid playlist or video id")
+    }
+    const playlist = await Playlist.findById(playlistId)
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    const isVideoInPlaylist = await playlist.videos.includes(videoId)
+    if (isVideoInPlaylist) {
+        throw new ApiError(400, "Video already in playlist")
+    } else if(playlist.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "You are not authorized to add video to this playlist")
+    } else {
+        playlist.videos.push(videoId)
+        await playlist.save()
+    }
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, playlist, "Video added to playlist successfully"))
+    )
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
+    if (!isValidObjectId(playlistId) || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid playlist or video id")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    const isVideoInPlaylist = await playlist.videos.includes(videoId)
+    if (!isVideoInPlaylist) {
+        throw new ApiError(400, "Video not in playlist")
+    } else if(playlist.owner.toString() !== req.user?._id.toString()) { 
+        throw new ApiError(403, "You are not authorized to remove video from this playlist")
+    } else {
+        playlist.videos.pull(videoId)
+        await playlist.save()
+    }
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, playlist, "Video removed from playlist successfully"))
+    )
 
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist id")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    if(playlist.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this playlist")
+    } else {
+        await playlist.remove()
+    }
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, null, "Playlist deleted successfully"))
+    )
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist id")
+    }
+    if (!name) {
+        throw new ApiError(400, "Name is required")
+    }
+    const playlist = await Playlist.findById(playlistId)
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    const user = await User.findById(req.user?._id)
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+    playlist.name = name
+    playlist.description = description
+    const updatedPlaylist = await playlist.save()
+
+    
+    // const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    //    playlistId,
+    //    {
+    //          $set:{
+    //           name: name,
+    //          description: description
+    //          }
+    //    },
+    //    {
+    //        new: true
+    //    }
+    //)
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200, updatedPlaylist, "Playlist updated successfully"))
+    )
 })
 
 export {
