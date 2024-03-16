52347010e4ae9b55a7b5b42e223b00511c459c8c
BASHIR AHMED MONDAL <bashirmondal03@gmail.com>
2024-03-16 11:00:50 +0530

    Add comment and dashboard controllers

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -3,29 +3,131 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+ 
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id")
+    }
+
+    const comments = await Comment.find({ videoId: videoId })
+        .limit(parseInt(limit))
+        .skip((parseInt(page) - 1) * parseInt(limit))
+        .exec();
+
+    const count = await Comment.countDocuments({ videoId: videoId });
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse({
+            comments,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page
+            })
+        )
+    )
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const videoId = req.params
+    const content = req.body
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id")
+    }
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    const comment = await  Comment.create({
+        content: content,
+        video:video._id,
+        owner: req.user._id
+    })
+
+    if (!comment) {
+        throw new ApiError(500, "Error adding comment")
+    }
+
+    return(
+        res
+        .status(201)
+        .json(new ApiResponse(200,comment, "Comment added successfully"))
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const commentId = req.params
+    const content = req.body
+    
+
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id")
+    }
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+    const user = req.user._id
+    if (!user) {
+        throw new ApiError(401, "You must be logged in to update a comment")
+    }
+   
+    if (comment?.owner.toString() !== user.toString()) {
+        throw new ApiError(403, "You are not authorized to update this comment")
+    }
+
+
+    comment.content = content
+    await comment.save({validateBeforeSave: false})
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200,comment, "Comment updated successfully"))
+    )
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const commentId = req.params
+    const user = req.user._id
+    if (user) {
+        throw new ApiError(401, "You must be logged in to delete a comment")
+    }
+
+    if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id")
+    }
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+    if (comment?.owner.toString() !== user.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this comment")
+    }
+
+    await comment.remove()
+
+    return(
+        res
+        .status(200)
+        .json(new ApiResponse(200,{}, "Comment deleted successfully"))
+    )
 })
 
 export {
     getVideoComments, 
     addComment, 
     updateComment,
-     deleteComment
+    deleteComment
     }
--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -8,10 +8,98 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const {channelId} = req.params
+    if (!mongoose.isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const channel = await Subscription.findbyId(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const totalViews = await Video.aggregate([
+        {
+            $match: {
+                owner: mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: {
+                    $sum: "$views"
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                totalViews: 1
+            }
+        }
+    ])
+
+    //Total subscribers
+    const totalSubscribers = await Subscription.countDocuments({
+        channel: channelId
+    })
+
+    //Total videos
+    const totalVideos = await Video.countDocuments({
+        owner: channelId
+    })
+
+    //Total likes
+    const totalLikes = await Like.countDocuments({
+        video: {
+            $in: await Video.find({
+                owner: channelId
+            }).distinct("_id")
+        }
+    })
+
+    return(
+        res
+        .status(200)
+        .json(
+            new ApiResponse(200, {
+                totalViews: totalViews[0]?.totalViews || 0,
+                totalSubscribers,
+                totalVideos,
+                totalLikes
+            }, "Channel stats fetched successfully")
+        )
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const {channelId} = req.params
+    if (!mongoose.isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const channel = await Subscription.findbyId(channelId)
+    if (!channel) {
+        throw new ApiError(404, "Channel not found")
+    }
+
+    const videos = await Video.find({
+        owner:channelId
+    }).sort("-createdAt")
+
+    if (!videos || videos.length === 0) {
+        throw new ApiError(404, "No videos found")
+    }
+
+    return(
+        res
+        .status(200)
+        .json(
+            new ApiResponse(200, videos, "Videos fetched successfully")
+        )
+    )
 })
 
 export {
